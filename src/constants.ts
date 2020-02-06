3d6235cd7ac62ad69535d72fe8412a8eba46d13b
Ivan Gabriele <ivan.gabriele@gmail.com>
2020-02-06 12:12:00 +0100

    feat: deprecate `baseUri` option

--- src/constants.ts
+++
@@ -1,7 +0,0 @@
-import { PostgresterConfig } from "./types";
-
-export const DEFAULT_CONFIG: PostgresterConfig = {
-  axiosConfig: null,
-  axiosInstance: null,
-  baseUri: ""
-};
---
+++ tests/mocks/console.js
@@ -0,0 +1,7 @@
+global.console = {
+  debug: console.debug,
+  error: jest.fn(),
+  info: jest.fn(),
+  log: console.log,
+  warn: jest.fn()
+};
