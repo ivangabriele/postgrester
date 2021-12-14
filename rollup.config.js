782c51f4024d01d9966c34792e1e5caec953b465
Ivan Gabriele <ivangabriele@users.noreply.github.com>
2021-12-14 18:14:00 +0100

    build(rollup): migrate to official plugins (#206)

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
-import cleaner from 'rollup-plugin-cleaner'
-import commonjs from 'rollup-plugin-commonjs'
-import nodeResolve from 'rollup-plugin-node-resolve'
-import typescript from 'rollup-plugin-typescript2'
+import commonjs from '@rollup/plugin-commonjs'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript'
 
 export default {
   external: ['axios'],
@@ -18,8 +17,6 @@ export default {
   ],
 
   plugins: [
-    // Clean /dist directory:
-    cleaner({ targets: ['./dist'] }),
     nodeResolve({
       extensions: ['.js', 'json', '.ts', '.tson'],
     }),
